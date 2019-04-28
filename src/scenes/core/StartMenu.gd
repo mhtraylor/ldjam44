e89e8176ebfc1af8c41ccc381b949cbc4ddccf3c
Justin Vidler <justin.vidler@servicemaster.com>
2019-04-28 13:49:10 -0500

    start of sounds working

--- src/scenes/core/StartMenu.gd
+++ src/scenes/core/StartMenu.gd
@@ -1,11 +1,32 @@
 extends Node
 
+var sound
+var music
+var t
+
 func _ready():
-	pass # Replace with function body.
+	t = Timer.new()
+	t.connect("timeout", self, "_on_timer_timeout")
+	add_child(t)
+	sound = get_node("SelectSound")
+	music = get_node("MenuMusic")
+	music.play()
+	t.set_wait_time(1.5)
+
 
 #func _process(delta):
 #	pass
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
-		get_tree().change_scene("res://scenes/core/Main.tscn")
+		music.stop()
+		play_sound()
+		
+
+func play_sound():
+	sound.play()
+	t.start()
+	
+func _on_timer_timeout():
+	sound.stop()
+	get_tree().change_scene("res://scenes/core/Main.tscn")
