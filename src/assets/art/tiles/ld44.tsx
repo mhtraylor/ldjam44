9fb5f37d3ee502dcad6ee8a7bcb207c126a318cc
Zac Freeman <zac.freeman@ahs.com>
2019-04-28 17:35:50 -0500

    added basic world map, godot tileset

---
+++ src/assets/art/tiles/ld44.tsx
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ld44" tilewidth="32" tileheight="32" tilecount="14" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="wall_se_in_32x32.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="wall_sw_in_32x32.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="wall_nw_in_32x32.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="wall_ne_in_32x32.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="wall_right_32x32.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="wall_left_32x32.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="wall_bottom_32x32.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="wall_top_32x32.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="solid_dark_32x32.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="solid_light_32x32.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="wall_sw_out_32x32.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="wall_nw_out_32x32.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="wall_ne_out_32x32.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="wall_se_out_32x32.png"/>
+ </tile>
+</tileset>
